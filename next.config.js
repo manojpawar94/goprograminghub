cbdde3f974dfc4eeb0f937cb80da85de3f215e31
Manoj Pawae <mmpawar94@gmail.com>
2022-04-25 10:25:06 +0530

    Changes to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const debug = process.env.NODE_ENV !== 'production'
-
 module.exports = {
-    //assetPrefix: !debug ? '/goprogramminghub/' : '',
     images: {
         loader: 'akamai',
-        path: '',
+        path: '/',
     },
 }
