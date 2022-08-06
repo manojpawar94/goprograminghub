b4a7d19308667a3c230ef07f4f00e3a9511a4900
manojpawar94 <58629059+manojpawar94@users.noreply.github.com>
2022-08-06 03:59:05 +0000

    Deploying to gh-pages from @ manojpawar94/goprograminghub@8446683bac53c65f6b0baf391366e9c170be33f6 🚀

--- _next/static/xTQsuFh4uhMpHTrT4phX-/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fposts\u002Fgolang","\u002Fposts\u002Fpython","\u002Fposts\u002Fgolang\u002F[slug]","\u002Fposts\u002Fpython\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
