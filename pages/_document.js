4d65058f758bdec508c9a0fccefdc28ba2432a2f
Manoj Pawar <58629059+manojpawar94@users.noreply.github.com>
2022-09-15 07:41:22 +0530

    Update _document.js
    Adding Google Adsense Link

--- pages/_document.js
+++ pages/_document.js
@@ -1,41 +1,47 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
-    }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-    render() {
-        const meta = {
-            title: 'Go Programming Hub',
-            description: 'GoProgrammingHub.com is a golang tutorial helps on how to do problems. Also how to do in python.',
-            image:
-                'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
-        }
+  render() {
+    const meta = {
+      title: "Go Programming Hub",
+      description:
+        "GoProgrammingHub.com is a golang tutorial helps on how to do problems. Also how to do in python.",
+      image:
+        "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+    };
 
-        return (
-            <Html lang="en">
-                <Head>
-                    <meta name="robots" content="follow, index" />
-                    <meta name="description" content={meta.description} />
-                    <meta property="og:site_name" content={meta.title} />
-                    <meta property="og:description" content={meta.description} />
-                    <meta property="og:title" content={meta.title} />
-                    <meta property="og:image" content={meta.image} />
-                    <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="twitter:site" content="@yourname" />
-                    <meta name="twitter:title" content={meta.title} />
-                    <meta name="twitter:description" content={meta.description} />
-                    <meta name="twitter:image" content={meta.image} />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="robots" content="follow, index" />
+          <meta name="description" content={meta.description} />
+          <meta property="og:site_name" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@yourname" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1364948475392350"
+            crossorigin="anonymous"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
